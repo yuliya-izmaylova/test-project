5a0013d0441dd78d5ee745fe805813ce26104078
yuliya-izmaylova <4479742+yuliya-izmaylova@users.noreply.github.com>
2025-02-24 08:10:18 +0200

    Update CountyCodeServiceInterface.php

--- app/Console/Commands/Services/CodeServices/CountyCodeServiceInterface.php
+++ app/Console/Commands/Services/CodeServices/CountyCodeServiceInterface.php
@@ -5,5 +5,5 @@ namespace App\Console\Commands\Services\CodeServices;
 interface CountyCodeServiceInterface
 {
     public function isEuCountry(string $bin): bool;
-    public function getCountryCode(string $bin): string;
+    public function getCountryCode(string $bin): ?string;
 }
